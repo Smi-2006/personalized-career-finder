a655d5131672d113556737420024711b58fdfe82
Smi-2006 <smithasrikumar6@gmail.com>
2026-01-07 21:48:34 +0530

    Update firebase.py

--- firebase.py
+++ firebase.py
@@ -1,7 +1,17 @@
+import os
+import json
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-cred = credentials.Certificate("serviceAccountKey.json")
+# Load Firebase key from environment variable
+firebase_key = os.environ.get("FIREBASE_KEY")
+
+if not firebase_key:
+    raise ValueError("FIREBASE_KEY environment variable not set")
+
+key_dict = json.loads(firebase_key)
+
+cred = credentials.Certificate(key_dict)
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
